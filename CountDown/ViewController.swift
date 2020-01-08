6197e38ae5b5f337aec2dc97b935d8b5670c4d24
hieu <itachipain11@gmail.com>
2020-01-08 14:30:39 +0700

    Initial Commit

---
+++ CountDown/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountDown
+//
+//  Created by Nguyễn Minh Hiếu on 1/8/20.
+//  Copyright © 2020 Nguyễn Minh Hiếu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
